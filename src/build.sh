288adc6da792db7afd2a0db0890c226331580312
Snoozeds <88577069+Snoozeds@users.noreply.github.com>
2023-11-22 21:27:19 +0000

    Add files via upload

---
+++ src/build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+DESTINATION_DIR="/usr/local/bin"
+
+# Build
+dotnet build
+
+# Publish the application
+dotnet publish -r linux-x64 -p:PublishSingleFile=true --self-contained false -c Release
+
+# Copy the application to /usr/local/bin/
+sudo cp bin/Release/net6.0/linux-x64/publish/Yanta $DESTINATION_DIR
+echo ">> Moved binary to $DESTINATION_DIR"
