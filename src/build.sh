3fda90cf6d102e0e00a7604dca2350fc8f1c2c2b
Snoozeds <88577069+Snoozeds@users.noreply.github.com>
2023-11-22 21:27:49 +0000

    Update build.sh

--- src/build.sh
+++ src/build.sh
@@ -8,6 +8,6 @@ dotnet build
 # Publish the application
 dotnet publish -r linux-x64 -p:PublishSingleFile=true --self-contained false -c Release
 
-# Copy the application to /usr/local/bin/
+# Copy the application to specified destination
 sudo cp bin/Release/net6.0/linux-x64/publish/Yanta $DESTINATION_DIR
 echo ">> Moved binary to $DESTINATION_DIR"
